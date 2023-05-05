312032865fb8ef487bf64f851a5d86bbfd0b3766
Thomas Verschoren <thomas@verschoren.com>
2023-05-05 16:04:33 +0200

    Add alert

--- docs/js/main.js
+++ docs/js/main.js
@@ -16,4 +16,5 @@ $(document).ready(function() {
         <a href="#" class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">Marketplace</a>
         <a href="#" class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">Company</a>
     `);
+    
 });
