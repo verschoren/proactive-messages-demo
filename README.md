f2fceece3ef8b41b762defcebe6beb1ffde1bcb6
Thomas Verschoren <thomas@verschoren.com>
2023-05-05 20:06:32 +0200

    Fix loggin Flow

--- README.md
+++ README.md
@@ -10,3 +10,17 @@ See the [following article on Internal Note](url) for more info.
 - [] Set URL
 - [] Set Description
 - [] Upload Attachment for webviews 
+
+
+Jurassic park
+DEMO_User
+Proactive
+Messaging_French
+DEMO_VIP
+DEMO_Welcome
+DEMO_Product_Untagged
+DEMO_Product_Tagged
+
+- Add SSO Login to Corleone
+- Add SSO Login to Gladiator
+- Remove SSO Login on other pages
--- docs/js/main.js
+++ docs/js/main.js
@@ -1,4 +1,3 @@
-//document ready
 $(document).ready(function() {
     $('.toggle_menu').click(function() {
         $('#sidebar').toggleClass('hidden');
@@ -20,12 +19,64 @@ $(document).ready(function() {
         <a href="contact.html#vip" class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">Contact VIP</a>
     `);
 
-    //if page url contains #vip
-    if (window.location.href.indexOf("#vip") > -1) {
-        $('#vip').removeClass('hidden');
-        $('#normal').addClass('hidden');
+    //if page url contains contact
+    if (window.location.href.indexOf("contact") > -1) {
+        Logout();
+        var random = randomString(16);
+        if (window.location.href.indexOf("#vip") > -1) {
+            Login({
+                name: 'Vito Corleone',
+                email:'vitocorleone+'+random+'@example.com',
+                external_id: random
+            });
+            $('#vip').removeClass('hidden');
+            $('#normal').addClass('hidden');
+        } else {
+            Login({
+                name: 'Maximus Decimus Meridius',
+                email:'maximus+'+random+'@example.com',
+                external_id: random
+            });
+            $('#normal').removeClass('hidden');
+            $('#vip').addClass('hidden');
+        }
     } else {
-        $('#normal').removeClass('hidden');
-        $('#vip').addClass('hidden');
+        Logout();
+    }
+
+    //JWT Messaging Code, see https://jwt.internalnote.com/messaging.html
+    function Logout(){
+        zE('messenger', 'logoutUser');
+    }
+    
+    function Login(user){
+        //get token for current user
+        var jwttoken = '';
+        $.ajax({
+          type: "POST",
+          url: 'https://jwt-demo.verschoren.workers.dev/messaging', //replace with your worker
+          data: JSON.stringify({ "external_id": user.external_id, "user_email": user.email, "user_name": user.name }),
+          dataType: 'text',
+          async: false,
+          success: function (json) {
+            jwttoken = json;
+          }
+        });
+        console.log(jwttoken)
+    
+        //authenticate messaging
+        zE('messenger', 'loginUser', function (callback) {
+          callback(jwttoken);
+        });
+      }
+
+      function randomString(length) {
+        var result = '';
+        var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+        var charactersLength = characters.length;
+        for (var i = 0; i < length; i++) {
+            result += characters.charAt(Math.floor(Math.random() * charactersLength));
+        }
+        return result;
     }
 });
